ec43073cce5810e1e5e3bebe59bb6df537078bb6
johnny7212 <jonddueno@gmail.com>
2023-03-14 14:34:58 -0500

    Initial commit

---
+++ src/CharSelect.java
@@ -0,0 +1,11 @@
+public class CharSelect {
+    private int health = 100;
+    private int atk = 100;
+
+    public CharSelect(int health, int atk) {
+        this.health = health;
+        this.atk = atk;
+    }
+
+
+}
